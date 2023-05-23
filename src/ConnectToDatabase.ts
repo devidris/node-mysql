a3a142ad75e9cb1106735cd3f5d41719e0d37553
Idris <info@dev-idris.com>
2023-05-24 00:32:29 +0100

    connection to database functionality implemented

---
+++ src/ConnectToDatabase.ts
@@ -0,0 +1,31 @@
+const mysql = require("mysql2");
+function ConnectToDatabase(
+  host: string,
+  user: string,
+  password: string,
+  database: string
+) {
+  if(!host || host === ''){
+    throw new Error("Host is not defined")
+  }
+  if(!user || user === ''){
+    throw new Error("Username is not defined")
+  }
+  if(!password || password === ''){
+    throw new Error("Password is not defined")
+  }
+  if(!database || database === ''){
+    throw new Error("Database is not defined")
+  }
+  return mysql.createPool({
+    host,
+    user,
+    password,
+    database,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+}
+
+module.exports = ConnectToDatabase;
